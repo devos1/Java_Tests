60d4b21d5480635efb027fa67cff6f8b275a1726
devos1 <oscardasilva@192.168.1.5>
2014-11-15 19:06:56 +0100

    Initial commit

---
+++ IMC/src/Imc.java
@@ -0,0 +1,57 @@
+/*******************************************
+ * Completez le programme a partir d'ici.
+ *******************************************/
+
+class Patient{
+	
+	private double masse;
+	private double hauteur;
+	
+	public void init(double m, double h){
+		if(m > 0 && h > 0){
+			masse = m;
+			hauteur = h;
+		}else{
+			masse = 0;
+			hauteur = 0;
+		}
+	}
+	
+	public void afficher(){
+		//Patient : <poids> kg pour <taille> m
+		System.out.printf("Patient : %.1f kg pour %.1f m \n", masse, hauteur);
+	}
+	
+	public double poids(){
+		return masse;
+	}
+	
+	public double taille(){
+		return masse;
+	}
+	
+	public double imc(){
+		if(hauteur > 0){
+			return masse/(hauteur*hauteur);
+		}else{
+			return 0;
+		}
+	}
+	
+	
+}
+
+/*******************************************
+ * Ne rien modifier apres cette ligne.
+ *******************************************/
+class Imc {
+    public static void main(String[] args) {
+
+        Patient quidam = new Patient();
+        quidam.init(74.5, 1.75);
+        quidam.afficher();
+        System.out.println("IMC : " + quidam.imc());
+        quidam.init( -2.0, 4.5);
+        quidam.afficher();
+    }
+}
